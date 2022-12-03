6ac708b0ae674387963b98fc88a24879b61ccc66
Danis Sharipov <danshadance@mail.ru>
2022-12-04 00:43:34 +0600

    Сумма чисел от 1 до А

---
+++ task2/task24.cs
@@ -0,0 +1,34 @@
+﻿// Принимаем число
+// Выдаем сумму чисел от 1 до А
+
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    int num = 0;
+    while (index < length)
+    {
+        collection[index] = num + 1;
+        index++;
+        num++;
+    }
+}
+
+Console.WriteLine("Данная программа принимает число А и на выходе выдает сумму чисел от 1 до А");
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [number];
+
+FillArray(array);
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum = sum + array[i];
+}
+
+Console.WriteLine("Сумма чисел = " + sum);
+
+// !!! .НАДО СРОЧНО РАЗОБРАТЬСЯ С МЕТОДАМИ. !!!
