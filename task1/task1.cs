3561263e0228a6f00a03e6dfea6bc675afeb9fb1
Danis Sharipov <danshadance@mail.ru>
2022-12-03 22:27:45 +0600

    59:17 семинар 4

---
+++ task1/task1.cs
@@ -0,0 +1,19 @@
+﻿// Что такое методы и для чего они нужны
+// Нам нужно вывести на экран имя языка
+
+void SayLang(string name)
+{
+    Console.WriteLine($"Язык {name}"); // $ - Интерполяция строк!
+}
+
+SayLang("JS");
+
+SayLang("Python");
+
+SayLang("C#");
+
+SayLang("Java");
+
+string lang = "Russian"; // Передеаем иную переменную string
+
+SayLang(lang); // Получаем на выходе переменну string, которую задали выше, а не ту которая указана в методе 
