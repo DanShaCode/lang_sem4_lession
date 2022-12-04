5d01276d215068adfbaf48d8863c0ed7e4fbf77b
Danis Sharipov <danshadance@mail.ru>
2022-12-04 07:58:18 +0600

    Доработка Задача26

--- task26/task26.cs
+++ task26/task26.cs
@@ -1,15 +1,8 @@
 ﻿// Принимаем на вход число
 // Выдаем количество цифр в числе
 
-// double num1 = 123 / 10; // 1
-// double num2 = 12 / 10; // 2
-// double num3 = 1 / 10; // 3
-
-// Console.WriteLine(num1);
-// Console.WriteLine(num2);
-// Console.WriteLine(num3);
-
-int num = Convert.ToInt32(Console.ReadLine());
+void CountNum(int num)
+{
 int count = 1;
 if (num > 9)
 {
@@ -19,7 +12,7 @@ if (num > 9)
     count++;
     if (num <= 1)
     {
-        Console.WriteLine(count);
+        Console.WriteLine("Количество цифр в числе: " + count);
     }
 }
 }
@@ -28,6 +21,13 @@ else
     int one = 1;
     if (num > 0 && num < 10)
     {
-        Console.WriteLine(one);
+        Console.WriteLine("Количество цифр в числе: " + one);
     }
 }
+}
+
+Console.WriteLine("Данная программа принимает число и на выходе выдает количество цифр в числе.");
+Console.Write("Пожалуйста, введите любое целое число больше 0: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CountNum(num);
